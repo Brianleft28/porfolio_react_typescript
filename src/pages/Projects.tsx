899357ca9a9015420389af65b50b163da6af47ce
Brianleft28 <contactobrianleft@gmail.com>
2024-09-28 17:45:53 -0300

    update: changes in the project page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -5,14 +5,23 @@ import CardProject from "../components/features/Projects/CardProject";
 import { projects } from "../components/features/Projects/Texts";
 import { Project } from "../types";
 import Modal from "../components/common/Modal";
+import { CategoryContext } from "../context/Category";
 
 const Projects = () => {
   const { lang } = useContext(LangContext);
+  const { category } = useContext(CategoryContext);
+
+  const filteredProjects = projects.filter((project: Project) => {
+    if (category === 'all') return true;
+    if (category === '') return true;
+    return project[lang].category === category;
+  } );
 
   return (
     <Wrapper lang={lang}>
       <div className="grid p-5 sm:grid-cols-2 grid-cols-1 md:grid-cols-3 gap-2">
-        {projects.map((project: Project, i: number) => (
+
+        {filteredProjects.map((project: Project, i: number) => (
           <div
             key={project[lang].key + i}
             className="mx-3 rounded-sm shadow-sm  bg-gradient-to-b from-content1 max-w-[450px] p-4 hover:cursor-pointer hover:-translate-y-1 hover:shadow-md rounded-e-md transition-all duration-250 hover:border-b-secondary border-b-primary border-b-2"
